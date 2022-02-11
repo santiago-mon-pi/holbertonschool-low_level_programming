2a4daed52caad77cf3f401be268923a3e50141a9
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-10 23:21:20 -0500

    corrected C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void)
+/*
+ * main function - Entry.
+ *
+ * return: Always: 0 (Success)
+ */
+
+int main (void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle ");
