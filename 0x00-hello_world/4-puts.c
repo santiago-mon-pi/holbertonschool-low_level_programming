7804700f2bb120a97e5181db3e2f4b3b77aeffd2
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-10 22:41:07 -0500

    added my first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
-#include <stdio.h
-/**
- * 4-puts.c
- * santiag-mon-pi
- */
+#include <stdio.h>
+
 int main(void)
+
 {
-	puts(""Programming is like building a multilingual puzzle,\n");
-        return 0;
+	puts("\"Programming is like building a multilingual puzzle, ");\
+return 0;
 }
