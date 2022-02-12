0f29eb1af5ee616ea6a9ace8cf0e44c6b4fd1c83
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-11 23:21:17 -0500

    corrected code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,16 +15,16 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%is positive\n");
+		printf("%d is positive\n");
 	}
 
 	else if (n < 0)
 	{
-		printf("%is negative\n");
+		printf("%d is negative\n");
 	}
 	else
 	{
-		printf("%is zero\n");
+		printf("%d is zero\n");
 	}
 	return (0);
 }
