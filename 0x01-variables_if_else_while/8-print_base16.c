43af795c7d3e6a65db6c7b7976a2fa4bf1c0bcbf
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-13 12:45:12 -0500

    created program to print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success(
+ */
+
+int main(void)
+
+{
+	char a;
+	char n;
+
+	for (n = '0' ; n <= '9' ; n++)
+
+		putchar(number);
+
+	for (a = 'a'; a <= 'f'; a++)
+
+		putchar(a);
+
+	putchar('\n');
+	return (0);
+}
