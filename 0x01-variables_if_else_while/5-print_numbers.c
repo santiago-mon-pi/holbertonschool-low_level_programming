4325f23bc18be8907d07b64f0cd511e9d3e118db
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-13 11:35:52 -0500

    added program to print all single digits with base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+**/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+}
+putchar('\n');
+return (0);
+}
