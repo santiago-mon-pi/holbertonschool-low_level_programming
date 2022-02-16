b17b2166b05d5e3181849be3158bac69a46453ad
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-15 23:57:15 -0500

    added program

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @x: input
+ * Return: varies
+ */
+
+int print_last_digit(int x)
+
+{
+	int a;
+
+	a = x % 10;
+
+	if (a < 0)
+	{
+		a = a * -1;
+	}
+	return (a);
+}
