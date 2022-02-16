a53acd455aeab572e160245a32f50784a428464f
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-15 23:08:43 -0500

    added program to compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ * @z: input
+ * Return: varies
+ */
+
+int _abs(int z)
+{
+	int x;
+
+	if (z > 0)
+	{
+		x = z;
+	}
+	else if (z < 0)
+	{
+		x = z * -1;
+	}
+	return (x);
+}
