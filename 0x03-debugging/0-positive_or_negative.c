b77043bc649d219d1558e19d6bda77c3d7f66e52
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-16 16:31:35 -0500

    added function file

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	int n = i;
+
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	else
+	{
+		printf("%i is zero\n", n);
+	}
+	return;
+}
