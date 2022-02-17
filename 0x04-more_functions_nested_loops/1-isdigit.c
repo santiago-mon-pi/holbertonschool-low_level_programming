aab6cbf4a489de5ac8391ada50e0f84bb83c6fc4
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-17 15:24:16 -0500

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isupper - checks if
+* @c: Variable that entry
+* Return: 1 or 0
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
