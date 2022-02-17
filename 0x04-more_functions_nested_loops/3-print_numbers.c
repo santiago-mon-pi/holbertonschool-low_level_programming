2cbce6d7099e1350c38942fff392da4ddb64ee75
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-17 16:18:03 -0500

    added program to print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_numbers - prints numbers
+* @x: variable to print
+* Return: 0
+*/
+
+void print_numbers(void);
+
+{
+	{
+	char x = '0';
+
+	while (x <= '9)
+
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar (\n);
+}
