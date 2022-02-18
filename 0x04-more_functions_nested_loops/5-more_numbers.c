16ac3d19b1316d3fb06b25f781d21ef699b0d6c8
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-17 19:49:59 -0500

    program that prints digits x10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints digits from 0 to 14 ten times
+ */
+
+void more_numbers(void);
+
+{
+	int n;
+	char di;
+
+/*loop counts to ten*/
+
+	for (n = 0; n < 10; ++n
+		)
+	{
+		/*inner loop prints digits from '0' to '14'*/
+
+		for (di = '0'; di <= '14'; ++ch)
+
+		{
+			_putchar(di);
+
+		}
+
+		_putchar('\n');
+
+	}
+
+return;
+
+}
