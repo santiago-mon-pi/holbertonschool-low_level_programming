7214583f9b3f83e15be7def881659cf14ffe52ab
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-17 19:56:46 -0500

    corrected code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,11 @@ void more_numbers(void)
 	int n;
 	int di;
 
-/*loop counts to ten*/
-
 	for (n = 0; n < 10; ++n)
-	{
-		/*inner loop prints digits from '0' to '14'*/
 
-		for (di = '0'; di <= '14'; ++di)
+	{
+
+		for (di = '0'; di <= "14"; ++di)
 
 		{
 			_putchar(di);
