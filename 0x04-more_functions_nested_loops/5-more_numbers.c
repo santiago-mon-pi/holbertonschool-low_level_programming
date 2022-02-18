f4cfd40cb8deeb621c158775de42d57c3bb6797a
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-17 22:29:42 -0500

    corrected code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,13 @@ void more_numbers(void)
 	int n;
 	char d;
 
-	for (n = 0; n < 10; ++n)
+	for (n = 0; n < 10; n++)
 
 	{
-
 		for (d = 0; d <= 14; d++)
 
 		{
-			if (c > 9)
-			_putchar((d / 10) + '0');
-			_putchar((d % 10) + '0');
+			_putchar(d)
 		}
 
 		_putchar('\n');
