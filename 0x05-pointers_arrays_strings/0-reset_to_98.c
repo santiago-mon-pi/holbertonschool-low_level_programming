2ec7c6b3197785870d58a1df4c681bd37edb71a9
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-21 17:32:05 -0500

    added program to change pointer value and header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - changes pointer value
+ *@n: variable
+ */
+
+void reset_to_98(int *n)
+
+* n = 98;
+
+return (o);
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  *function prototypes go down here
+  */
+
+void reset_to_98(int *n);
+
+#endif
