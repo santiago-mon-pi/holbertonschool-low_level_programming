b0a9b14946295ebd1f941bd775777f480d88e719
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-21 21:01:05 -0500

    added function to reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* rev_string - reverses string
+* @s: string to reverse
+* Return: reversed string
+*/
+
+void rev_string(char *s)
+
+{
+	char rev = s[0];
+
+	int count = 0;
+
+	int x;
+
+	while (s[count] != '\0')
+
+		count++;
+
+	for (x = 0; x < count; x++)
+
+	{
+		counter--;
+		rev = s[x];
+		s[x] = s[count];
+		s[count] = rev;
+	}
+}
