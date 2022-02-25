a24c8869d84e72504f9ce44dc37679f9479b95c7
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-24 19:46:59 -0500

    corrected code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,20 +9,17 @@
 char *_strcat(char *dest, char *src)
 
 {
-	int i, int j;
+	int i, j;
 	for (i = 0; dest[i] != '\0'; i++)
 
 	{
 	}
 
-	for (j = 0; src[j] != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++, i++)
 
 	{
-		i++;
 		dest[i] = src[j];
 	}
-
-	dest[i + 1] = '\0';
 
 	return (dest);
 }
