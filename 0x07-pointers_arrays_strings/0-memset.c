30096f8e14428e2cd59dbf62dbcfab777275d0f9
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-28 20:40:41 -0500

    corrected code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *_memset - fill block memory with specific value
@@ -9,10 +10,8 @@
  */
 
 char *_memset(char *s, char b, unsigned int n)
-
 {
-	unsingned int i;
-
+	unsigned int i;
 	for (i = 0; i < n; i++)
 	s[i] = b;
 	return (s);
