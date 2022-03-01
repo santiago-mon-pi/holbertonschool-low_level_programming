1fbbfdc34768e71c0722fb42d50b1115465584cb
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-02-28 20:00:51 -0500

    added memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_memset - fill block memory with specific value
+ *@s: pointer
+ *@n: number of bytes
+ *@b: input
+ *Return: return s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	int i;
+
+	i = 0;
+
+	for (i < n; i++;)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
