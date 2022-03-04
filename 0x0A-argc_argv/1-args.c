aad22ab62ea27648b63613b44091429636259bcc
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-03-04 12:30:14 -0500

    added program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print number of arguments
+ * @argc: number
+ * @argv: Pointer array
+ *
+ * Return: 0 to success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
