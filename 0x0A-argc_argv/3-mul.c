a111da10c12330e481e10308da1ee9a8691763c5
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-03-04 13:24:07 -0500

    added program to multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * main - mult the 1 to the 3 number
+ * @argc: number
+ * @argv: Pointer array
+ * @m: multiplication
+ * Return: 0 to success
+ */
+
+int main(int argc, char *argv[])
+{
+	int m;
+
+	if (argc == 3)
+	{
+		m = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
