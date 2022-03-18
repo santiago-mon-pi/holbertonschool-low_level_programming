73d58fde2346c14ddcd7b55fa2a87ac7853f33a1
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-03-18 14:11:48 -0500

    added runction to print numbers followed by a new line

---
+++ 0x10-varidic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints numbers n
+ * @separator: Separator string
+ * @n: n integer to print
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list l;
+	unsigned int i;
+
+	va_start(l, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(l, int));
+		if (separator == NULL)
+		{
+			continue;
+		}
+		else if (i == (n - 1))
+		{
+			continue;
+		}
+		else
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(l);
+}
